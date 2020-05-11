34dfade49a5844a9a0b579d916a02aef817f7300
MaxIbanez <m.ibaezmolina@uandresbello.edu>
2020-05-11 18:11:55 -0400

    pass en consola/archivo.txt

--- gcp.py
+++ gcp.py
@@ -17,19 +17,24 @@ class password():
 		self.printInit()
 		self.logicaPrograma()
 	def entrada(self):
-		print ("\nSe recomienda una contraseña de mínimo 8 caracteres")
-		cant = int(input("Tamaño del password a crear:"))
+		print ("\n\tSe recomienda una contraseña de mínimo 8 caracteres")
+		cant = int(input("\tTamaño del password a crear: "))
 		return cant
 	def caracter(self):
 		char = chr(rd.randint(33,126))
 		return char
+	def misClaves(self, clave):
+		archivo = open("history.txt","a")
+		archivo.write("password:"+clave+"\n")
+		print("Clave guardada correctamente")
+
 	def printInit(self):
 		print("\t====================================")
 		print("\t Generador de contraseñas aleatorio\n\t \t \tby\n\t \tMaximiliano Ibáñez")
 		print("\t====================================")
 	def logicaPrograma(self):
 		self.cant = self.entrada()
-		while self.cant<0 or self.cant >128:
+		while self.cant<4 or self.cant >128:
 			print ("Favor ingresar un numero entre 0 y 128\n")
 			cant = self.entrada()
 		conteo = 0
@@ -39,8 +44,17 @@ class password():
 				char = self.caracter()
 			self.clave = self.clave+char
 			conteo+=1
-		print ("\nContraseña creada exitosamente: ")
-		print ("\n"+self.clave)
+		print ("\nContraseña creada exitosamente.\n\nDesea ver en consola(1)\n\to\nver en consola y guardar en archivo(0)")
+		opcion = int(input())
+		while opcion <0 and opcion>1:
+			opcion = int(input("Ingrese opción nuevamente: "))
+		if(opcion == 1 ):
+			print ("\n"+self.clave)
+		else:
+			print ("\n"+self.clave)
+			self.misClaves(self.clave)
+
+
 
 new = password()
 new.__main__()
